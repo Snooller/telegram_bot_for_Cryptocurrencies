d779723afd121a516464a9ce66e4d7ff5a0a3dfb
Snooller <33541559+Snooller@users.noreply.github.com>
2023-01-23 15:45:39 +0300

    Create vcs.xml

--- handlers/config.py
+++ handlers/config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKENN= str(os.getenv("BOT_TOKENN"))
+BOT_TOKENN= str(os.getenv("5722903855:AAEB7SrRKv7LUEwoz1FY9WU-dvHnPAB8HAQ"))
 
 admins={
-    916384985
+    439963689
 }
